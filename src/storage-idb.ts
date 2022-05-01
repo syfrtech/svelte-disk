a0cfb0cc7392f09a09da59e729412abc12dd744e
Jonathan Stanley <jonathan@bristleconeweb.com>
2022-04-30 21:19:34 -0400

    feat: enable custom store name

--- src/storage-idb.ts
+++ src/storage-idb.ts
@@ -1,4 +1,4 @@
-import { get, set, del, keys } from "idb-keyval";
+import { get, set, del, keys, createStore } from "idb-keyval";
 import {
   type StorageInterface,
   type StorageContainer,
@@ -8,21 +8,30 @@ import {
 /**
  * Creates an Indexed DB persister
  * @see https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API
+ * @see https://github.com/jakearchibald/idb-keyval/blob/main/custom-stores.md#defining-a-custom-database--store-name
+ *
+ * @param {IDBValidKey} idbValidKey a name for the Svelte store
+ * @param {string} dbName the of the database
  */
-export function createIDBPersister<T>(idbValidKey: IDBValidKey) {
+export function createIDBPersister<T>(
+  idbValidKey: IDBValidKey,
+  dbName: string = "svelte-storestore"
+) {
   if (typeof window === "undefined") {
     return noopStorage<T>();
   } else {
+    let customStore = createStore(dbName, dbName);
     return {
       get: async () => {
-        let result = await get<StorageContainer<T>>(idbValidKey);
+        let result = await get<StorageContainer<T>>(idbValidKey, customStore);
         if (result === undefined) {
           if (!(await keys()).includes(idbValidKey)) throw "nonexistent key";
         }
         return result;
       },
-      set: async (packed: StorageContainer<T>) => set(idbValidKey, packed),
-      del: async () => del(idbValidKey),
+      set: async (packed: StorageContainer<T>) =>
+        set(idbValidKey, packed, customStore),
+      del: async () => del(idbValidKey, customStore),
     } as StorageInterface<T>;
   }
 }
