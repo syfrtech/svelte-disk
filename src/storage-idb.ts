dce859efa626e9ee8ddbf2b65e73189c0c629d40
Jonathan Stanley <jonathan@bristleconeweb.com>
2022-04-30 21:07:36 -0400

    fix: attempt browser check fix

--- src/storage-idb.ts
+++ src/storage-idb.ts
@@ -10,18 +10,19 @@ import {
  * @see https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API
  */
 export function createIDBPersister<T>(idbValidKey: IDBValidKey) {
-  if (!window) {
+  if (typeof window === "undefined") {
     return noopStorage<T>();
+  } else {
+    return {
+      get: async () => {
+        let result = await get<StorageContainer<T>>(idbValidKey);
+        if (result === undefined) {
+          if (!(await keys()).includes(idbValidKey)) throw "nonexistent key";
+        }
+        return result;
+      },
+      set: async (packed: StorageContainer<T>) => set(idbValidKey, packed),
+      del: async () => del(idbValidKey),
+    } as StorageInterface<T>;
   }
-  return {
-    get: async () => {
-      let result = await get<StorageContainer<T>>(idbValidKey);
-      if (result === undefined) {
-        if (!(await keys()).includes(idbValidKey)) throw "nonexistent key";
-      }
-      return result;
-    },
-    set: async (packed: StorageContainer<T>) => set(idbValidKey, packed),
-    del: async () => del(idbValidKey),
-  } as StorageInterface<T>;
 }
